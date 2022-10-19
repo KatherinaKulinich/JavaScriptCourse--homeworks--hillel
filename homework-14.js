b3fadfc7a24bd5c087928845dc5cd9f7221957a2
KatherinaKulinich <kulinichcatherina@gmail.com>
2022-10-19 17:15:05 +0300

    hw-4

---
+++ homework-14.js
@@ -0,0 +1,22 @@
+//Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length
+
+
+
+function generateKey(length, characters) {
+    let arrayCharacters = Array.from(characters);
+    let newArray = [];
+
+    for (let k = 0; k < length; k++) {
+        let min = 0;
+        let randomIndex;
+        randomIndex = Math.floor(Math.random() * (arrayCharacters.length - min + 1) ) + min;
+        newArray.push(arrayCharacters[randomIndex]);
+    }    
+    return newArray.join('');
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters); 
+
+console.log(key);
+    
