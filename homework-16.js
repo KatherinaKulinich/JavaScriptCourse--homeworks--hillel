f9148fb624b607c4c78b6e859143b69266adc10f
KatherinaKulinich <kulinichcatherina@gmail.com>
2022-10-22 16:58:11 +0300

    hw-16 fixed

--- homework-16.js
+++ homework-16.js
@@ -1,6 +1,7 @@
 function pow(num, degree) {
-    if (degree === 1) {
-        return num;
+    
+    if (degree === 0) {
+        return 1;
     } else {
         return num * pow(num, degree - 1)
     }
@@ -11,3 +12,6 @@ console.log(result);
 
 // let result1 = pow(4,5);
 // console.log(result1);
+
+// let result3 = pow(2,1);
+// console.warn(result3);
