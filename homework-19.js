36cb4cc6466418f2ace2c5f0b1b2f1b3e02f0829
KatherinaKulinich <kulinichcatherina@gmail.com>
2022-11-02 22:12:51 +0200

    corrects hw19

--- homework-19.js
+++ homework-19.js
@@ -4,138 +4,210 @@
 
 
 
-console.log('=======persons========');
-
 class Person {
-    constructor(name, surname, age, job) {
+    name;
+    gender;
+
+    constructor(name, gender) {
         this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.job = job;
+        this.gender = gender;
     }
 
-    fullName() {
-        return `${this.name} ${this.surname}`
+    getFullInfo() {
+        return `${this.name} (${this.gender})`
     }
 }
-
-const lena = new Person('Olena', 'Ivanova', 30, 'teacher');
-console.warn(lena);
-console.log(lena.fullName());
-
-const boris = new Person('Boris', 'Petrov', 28, 'manager');
-console.warn(boris);
-console.log(boris.fullName());
-
-const anna = new Person('Anna', 'Kolesnik', 41, 'doctor');
-console.warn(anna);
-console.log(anna.fullName());
-
-
-console.log('======flats=======');
 
 
 
 class Flat {
-    constructor(rooms, floor, persons) {
-        this.rooms = rooms;
-        this. floor = floor;
-        this.persons = 0;
+    residents = [];
+    heating = 'central';
+    walls = 'brick';
+    renovation = 'new';
+
+    addPerson(person) {
+        this.residents.push(person);
     }
 
-    newPersonInFlat() {
-        this.persons++;
-        return this;
-    }
-
-    get descriptionFlat() {
-        return `this flat on ${this.floor} floor with ${this.rooms} rooms, ${this.persons} persons live here`
+    showResidents() {
+        console.log('Residents of this flat:');
+        console.warn(this.residents);
     }
 }
-
-const flat1 = new Flat(2, 5, 3);
-
-console.log(flat1);
-console.warn(flat1.descriptionFlat);
-flat1.newPersonInFlat().newPersonInFlat().newPersonInFlat();
-console.log(flat1);
-console.warn(flat1.descriptionFlat);
-console.log('-----------');
-
-
-const flat2 = new Flat(3, 2, 2);
-
-console.log(flat2);
-console.warn(flat2.descriptionFlat);
-flat2.newPersonInFlat().newPersonInFlat().newPersonInFlat().newPersonInFlat().newPersonInFlat();
-console.log(flat2);
-console.warn(flat2.descriptionFlat);
-
-
-console.log('======house=======');
 
 
 class House {
+    flatsMax;
+    flatsOccupied = [];
+    location = 'center';
+    parking = 'yes';
+    floors = 6;
 
-    constructor(flatsOccupied, floors, resients) {
-        this.flatsAll = 30;
-        this.floors = floors;
-        this.resients = resients;
-        this.flatsOccupied = flatsOccupied;
+    constructor(flatsMax) {
+        this.flatsMax = flatsMax;
     }
 
-    newFlatOccupied() {
-        if (this.flatsOccupied >= this.flatsAll) {
-            console.warn('the house is full');
-            return;
+    addFlat(flat) {
+
+        if (this.flatsOccupied.length < this.flatsMax) {
+            this.flatsOccupied.push(flat);
+            return this.flatsOccupied;
         }
 
-        this.flatsOccupied++;
-        this.resients++;
-        return this;
+        return console.warn('house is full');
     }
 
-    get flats() {
-        if ((this.flatsAll - this.flatsOccupied) <= 0) {
-            return 'no free flats';
-        }
-
-        return `now ${this.flatsAll - this.flatsOccupied} flats are free`
+    showOccupiedFlats() {
+        console.log(this.flatsOccupied);
     }
 }
 
-const house1 = new House(25, 9, 44);
-console.warn(house1);
-console.log(house1.flats);
-
-house1.newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied();
-console.log(house1);
-console.log(house1.flats);
-
-house1.newFlatOccupied();
-house1.newFlatOccupied();
-house1.newFlatOccupied();
-console.log(house1);
-console.log('-----------');
 
 
-const house2 = new House(40, 12, 100);
-console.log(house2);
-house2.newFlatOccupied();
-console.log(house2);
-console.log('-----------');
+console.log('=======persons========');
 
+const lena = new Person('Olena', 'female');
+console.log(lena.getFullInfo());
 
-const house3 = new House(10, 5, 22);
-console.log(house3);
-console.log(house3.flats);
-house3.newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied();
-console.log(house3);
-console.log(house3.flats);
+const boris = new Person('Boris', 'male');
+console.log(boris.getFullInfo());
+
+const anna = new Person('Anna', 'female');
+console.log(anna.getFullInfo());
+
+const petro = new Person('Petro', 'male');;
+console.log(petro.getFullInfo());
+
+const vlad = new Person('Vlad', 'male');;
+console.log(vlad.getFullInfo());
 
 
 
+console.log('\n\n======flats=======');
 
+const twoRoomFlat = new Flat();
+// console.log(twoRoomFlat);
+
+twoRoomFlat.addPerson(boris);
+twoRoomFlat.addPerson(anna);
+twoRoomFlat.showResidents();
+
+
+const threeRoomFlat = new Flat();
+
+threeRoomFlat.addPerson(vlad);
+threeRoomFlat.addPerson(lena);
+threeRoomFlat.addPerson(petro);
+threeRoomFlat.showResidents();
+
+
+const flat1 = new Flat();
+const flat2 = new Flat();
+const flat3 = new Flat();
+const flat4 = new Flat();
+const flat5 = new Flat();
+const flat6 = new Flat();
+
+
+console.log('\n\n======house=======');
+
+const isSmallHouse = new House(5);
+// console.log(isSmallHouse);
+const isBigHouse = new House(8);
+
+isSmallHouse.addFlat(threeRoomFlat);
+isSmallHouse.addFlat(twoRoomFlat);
+isSmallHouse.addFlat(flat1);
+isSmallHouse.addFlat(flat2);
+isSmallHouse.addFlat(flat3);
+
+isSmallHouse.showOccupiedFlats();
+
+isSmallHouse.addFlat(flat4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class House {
+
+//     constructor(flatsOccupied, floors, resients) {
+//         this.flatsAll = 30;
+//         this.floors = floors;
+//         this.resients = resients;
+//         this.flatsOccupied = flatsOccupied;
+//     }
+
+//     newFlatOccupied() {
+//         if (this.flatsOccupied >= this.flatsAll) {
+//             console.warn('the house is full');
+//             return;
+//         }
+
+//         this.flatsOccupied++;
+//         this.resients++;
+//         return this;
+//     }
+
+//     get flats() {
+//         if ((this.flatsAll - this.flatsOccupied) <= 0) {
+//             return 'no free flats';
+//         }
+
+//         return `now ${this.flatsAll - this.flatsOccupied} flats are free`
+//     }
+// }
+
+// const house1 = new House(25, 9, 44);
+// console.warn(house1);
+// console.log(house1.flats);
+
+// house1.newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied();
+// console.log(house1);
+// console.log(house1.flats);
+
+// house1.newFlatOccupied();
+// house1.newFlatOccupied();
+// house1.newFlatOccupied();
+// console.log(house1);
+// console.log('-----------');
+
+
+// const house2 = new House(40, 12, 100);
+// console.log(house2);
+// house2.newFlatOccupied();
+// console.log(house2);
+// console.log('-----------');
+
+
+// const house3 = new House(10, 5, 22);
+// console.log(house3);
+// console.log(house3.flats);
+// house3.newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied().newFlatOccupied();
+// console.log(house3);
+// console.log(house3.flats);
 
 
 
